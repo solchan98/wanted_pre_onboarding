101d9388a35f86c3521f949d51cc2c56df4cb868
solchan98 <dev.sol0427@gmail.com>
2022-04-13 01:42:21 +0900

    refactor: 토글 버튼 width, height, font-size 입력받아 동적으로 크기 설정 가능하도록 리팩토링

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {Toggle} from "./components/toggle";
 function App() {
     return (
         <div className="App">
-            <Toggle />
+            <Toggle width={600} height={80} fontSize={24} />
         </div>
     );
 }
--- src/components/toggle.jsx
+++ src/components/toggle.jsx
@@ -3,55 +3,71 @@ import {useCallback, useState} from "react";
 
 const Backboard = styled.div`
   position: relative;
-  line-height: 30px;
+  line-height: ${state => state.height}px;
   display: flex;
-  width: 200px;
-  height: 30px;
-  border-radius: 100px;
+  width: ${state => state.width}px;
+  height: ${state => state.height}px;
+  border-radius: ${state => state.width / 2}px;
   background-color: #e8e6e6;
 `;
 
 const Circle = styled.div`
   transition: .2s;
-  top: 2px;
+  top: 4px;
   left: ${state => state.leftMargin}px;
   position: absolute;
-  border-radius: 40px;
-  width: 100px;
-  height: 26px;
+  border-radius: ${state => state.width - 8}px;
+  width: ${state => state.width / 2}px;
+  height: ${state => state.height - 8}px;
   z-index: 1;
   background-color: #FFF;
 `;
 
 const Item = styled.div`
   font-weight: bold;
+  font-size: ${state => state.fontSize}px;
+  overflow: hidden;
   text-align: center;
-  width: 200px;
-  height: 30px;
+  width: ${state => state.width}px;
+  height: ${state => state.height}px;
   z-index: 2;
-  opacity: ${state => state.clicked === state.keys ? "100%" : "30%" };
+  opacity: ${state => state.clicked === state.keys ? "100%" : "30%"};
   cursor: pointer;
 `;
 
-export const Toggle = () => {
+export const Toggle = ({width, height, fontSize}) => {
 
     const [clicked, setClicked] = useState(0);
-    const [leftMargin, setLeftMargin] = useState(2);
+    const [leftMargin, setLeftMargin] = useState(4);
 
     const click = useCallback((index) => {
-        if(index === 0) {
-            setLeftMargin(100 * index + 2);
+        if (index === 0) {
+            setLeftMargin(width / 2 * index + 4);
         } else {
-            setLeftMargin(100 * index - 2);
+            setLeftMargin(width / 2 * index - 4);
         }
         setClicked(index);
     }, []);
 
-    return(
-        <Backboard>
-            <Circle leftMargin={leftMargin} />
-            <Item clicked={clicked} keys={0} onClick={() => click(0)}>기본</Item>
-            <Item clicked={clicked} keys={1} onClick={() => click(1)}>상세</Item>
+    return (
+        <Backboard width={width} height={height}>
+            <Circle width={width} height={height} leftMargin={leftMargin}/>
+            <Item
+                width={width}
+                height={height}
+                clicked={clicked}
+                keys={0}
+                onClick={() => click(0)}
+                fontSize={fontSize}>기본
+            </Item>
+            <Item
+                width={width}
+                height={height}
+                clicked={clicked}
+                keys={1}
+                onClick={() => click(1)}
+                fontSize={fontSize}>상세
+            </Item>
         </Backboard>
     );
 }
