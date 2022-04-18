49462764da1cb9b75e9dbcb3c90a5796d3e533bc
solchan98 <dev.sol0427@gmail.com>
2022-04-18 21:50:37 +0900

    refactor: 전체 컴포넌트 App.js에 배치하도록 수정 및 자잘한 수정사항 반영

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
 import {Toggle} from "./components/toggle";
 import {Tab} from "./components/tab";
+import {Dropdown} from "./components/Dropdown";
+import {Input} from "./components/input";
+import {Slider} from "./components/slider";
+import {animals} from "./contstant/tab";
+import styled from "styled-components";
 
-const animals = [
-    { id: 0, name: "기린"},
-    { id: 1, name: "사슴"},
-    { id: 2, name: "고양이"},
-]
+const DIV = styled.div`
+    margin: 50px;
+`;
+
 
 function App() {
     return (
         <div className="App">
-            {/*<Toggle width={600} height={80} fontSize={24} />*/}
+            <Toggle width={200} height={40} fontSize={16} />
+            <DIV/>
             <Tab items={animals} width={100} height={50} fontSize={14} />
+            <DIV/>
+            <Slider width={400} height={50} />
+            <DIV/>
+            <Input width={300} height={40} fontSize={14}/>
+            <DIV/>
+            <Dropdown width={200} height={30} fontSize={14} />
         </div>
     );
 }
---
+++ src/contstant/tab.js
@@ -0,0 +1,5 @@
+export const animals = [
+    { id: 0, name: "기린"},
+    { id: 1, name: "사슴"},
+    { id: 2, name: "고양이"},
+]
